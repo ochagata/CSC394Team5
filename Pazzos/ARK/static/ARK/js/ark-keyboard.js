5d167c515b9836d00dc1beaefeef43c3da9be437
Christian Simpson <christian.simpson1291@gmail.com>
2014-06-12 05:30:10 -0500

    Added some logic related to examining the letter that was hit if within a certain range of the center of a div element. There's still some bugs to work out regarding the offset/position information of the child div elements (the ones that are supposed to represent the keys themselves), so once those are fixed, it's just a matter of adding each letter to the global letters array

--- Pazzos/ARK/static/ARK/js/ark-keyboard.js
+++ Pazzos/ARK/static/ARK/js/ark-keyboard.js
@@ -6,6 +6,7 @@
         dot_flag = false;
         sokgraph_x = [];
         sokgraph_y = [];
+        letters = [];
 
     var lineColor = "black",
         lineWidth = 2;
@@ -51,6 +52,91 @@
             prevX = currX;
             prevY = currY;
 
+            $(".row").each(function(index, element)
+            {
+                 alert($(this).attr('id'));
+                    var $this = $(this);
+                    var nowWidth = $this.width();
+                    var nowHeight = $this.height();
+                    var curOffset = $this.offset;
+                    var curLeft = $this.offset().left;
+                    var curTop = $this.offset().top;
+                    if(currX < curLeft + nowWidth && currX > curLeft)
+                    {
+                        if(currY < curTop + nowHeight && currY > curTop)
+                        {
+                            $this.children().each(function()
+                            {
+
+                                var curOffset = $(this).offset();
+                                var xCenter = curOffset.left + ($(this).width() / 2);
+                                var yCenter = curOffset.top + ($(this).height() / 2);
+                                if(currY < yCenter - 5 || currY > yCenter + 5 )
+                                {
+                                    if(currX < xCenter - 5 || currX > xCenter + 5)
+                                   {
+                                        letters.push($(this).attr('id'));
+                                       alert("Id: " + $(this).attr('id'));
+                                   }
+                                }
+                            });
+
+                        }
+                    }
+            });
+
+            //TODO: make 3 a computed value based on number of rows
+//            for(var i = 1; i <= 3; ++i)
+//            {
+//                var curRow = $('#row' + i);
+//                alert("Row class: " + curRow.attr('id'));
+//                $.each(curRow, function(index, element)
+//                {
+//                    alert($(this).attr('id'));
+//                    var now = $(this);
+//                    var nowWidth = now.width();
+//                    var nowHeight = now.height();
+//                    var curOffset = now.offset;
+//                    var curLeft = now.offset().left;
+//                    var curTop = now.offset().top;
+//                    if(currX < curLeft + nowWidth && currX > curLeft)
+//                    {
+//                        if(currY < curTop + nowHeight && currY > curTop)
+//                        {
+//                            alert("Id: " + now.attr('id'));
+//                        var curOffset = now.offset();
+//                        var xCenter = curOffset.left + (now.width() / 2);
+//                        var yCenter = curOffset.top + (now.height() / 2);
+//                        if(currY < yCenter - 5 || currY > yCenter + 5 )
+//                        {
+//                            if(currX < xCenter - 5 || currX > xCenter + 5)
+//                            {
+//                                letters.push(now.attr('id'));
+//                            }
+//                        }
+//                        }
+//                    }
+//
+////                    var $this = $(this);
+////                    $this.each(function(index, element)
+////                    {
+////                        var $secondThis = $(this);
+////                        alert("Id: " + $secondThis.attr('id'));
+////                        var curOffset = $secondThis.offset();
+////                        var xCenter = curOffset.left + ($secondThis.width() / 2);
+////                        var yCenter = curOffset.top + ($secondThis.height() / 2);
+////                        if(currY < yCenter - 5 || currY > yCenter + 5 )
+////                        {
+////                            if(currX < xCenter - 5 || currX > xCenter + 5)
+////                            {
+////                                letters.push($secondThis.attr('id'));
+////                            }
+////                        }
+////                    });
+//
+//                });
+//            }
+
             //when clicking on a point in the canvas, the x value gets pushed here, but the y value gets pushed here and
             //somewhere else
 //            sokgraph_x.push(currX);
@@ -72,12 +158,15 @@
         {
             if(bTrackCoords)
             {
+                setInterval(function(){}, 2000);
                 alert("X values: " + sokgraph_x);
                 alert("Y values: " + sokgraph_y);
                 alert("X values: " + sokgraph_x);
 
                 alert("X length: " + sokgraph_x.length);
                 alert("Y length: " + sokgraph_y.length);
+
+                alert("Letters: " + letters);
 
                 //get the arrays ready to store the next sokgraph and stop tracking their
                 //coordinates until they click again
