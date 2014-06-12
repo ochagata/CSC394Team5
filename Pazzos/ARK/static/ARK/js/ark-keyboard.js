0bdb8b1788700d4e73233cec3952900e9c715321
Christian Simpson <christian.simpson1291@gmail.com>
2014-06-12 01:22:48 -0500

    [Issue 6] Adding basic data collection to the keyboard and renamed some variables so they were more descriptive. For some reason, the mouse events are firing more than once, which leads to the x and y arrays to have different values. More investigation is needed to fix this problem. TODO: fix mouse firing bugs and use our coordinate data to find out what words are being spelled.

--- Pazzos/ARK/static/ARK/js/ark-keyboard.js
+++ Pazzos/ARK/static/ARK/js/ark-keyboard.js
@@ -1,12 +1,14 @@
-    var canvas, ctx, flag = false,
+    var canvas, ctx, bTrackCoords = false,
         prevX = 0,
         currX = 0,
         prevY = 0,
         currY = 0,
         dot_flag = false;
+        sokgraph_x = [];
+        sokgraph_y = [];
 
-    var x = "black",
-        y = 2;
+    var lineColor = "black",
+        lineWidth = 2;
 
     $(function() {
         canvas = document.getElementById('can');
@@ -29,50 +31,83 @@
     });
 
 
-    function draw() {
+    function draw()
+    {
         ctx.beginPath();
         ctx.moveTo(prevX, prevY);
         ctx.lineTo(currX, currY);
-        ctx.strokeStyle = x;
-        ctx.lineWidth = y;
+        ctx.strokeStyle = lineColor;
+        ctx.lineWidth = lineWidth;
         ctx.stroke();
         ctx.closePath();
     }
 
-    function findxy(res, e) {
-        if (res == 'down') {
-            prevX = currX;
-            prevY = currY;
+    function findxy(res, e)
+    {
+        if (res == 'down')
+        {
             currX = e.clientX - canvas.offsetLeft;
             currY = e.clientY - canvas.offsetTop;
+            prevX = currX;
+            prevY = currY;
 
-            flag = true;
+            //when clicking on a point in the canvas, the x value gets pushed here, but the y value gets pushed here and
+            //somewhere else
+//            sokgraph_x.push(currX);
+//            sokgraph_y.push(currY);
+
+            //the user has clicked, so we should start tracking their mouse movements
+            bTrackCoords = true;
             dot_flag = true;
-            if (dot_flag) {
+            if (dot_flag)
+            {
                 ctx.beginPath();
-                ctx.fillStyle = x;
+                ctx.fillStyle = lineColor;
                 ctx.fillRect(currX, currY, 2, 2);
                 ctx.closePath();
                 dot_flag = false;
             }
         }
-        if (res == 'up' || res == "out") {
-            flag = false;
+        if (res == 'up' || res == "out")
+        {
+            if(bTrackCoords)
+            {
+                alert("X values: " + sokgraph_x);
+                alert("Y values: " + sokgraph_y);
+                alert("X values: " + sokgraph_x);
+
+                alert("X length: " + sokgraph_x.length);
+                alert("Y length: " + sokgraph_y.length);
+
+                //get the arrays ready to store the next sokgraph and stop tracking their
+                //coordinates until they click again
+                sokgraph_x = [];
+                sokgraph_y = [];
+                bTrackCoords = false;
+
+            }
             erase();
         }
-        if (res == 'move') {
-            if (flag) {
+        if (res == 'move')
+        {
+            if (bTrackCoords)
+            {
                 prevX = currX;
                 prevY = currY;
                 currX = e.clientX - canvas.offsetLeft;
                 currY = e.clientY - canvas.offsetTop;
+                sokgraph_x.push(currX);
+                sokgraph_y.push(currY);
                 draw();
             }
         }
 
-        function erase() {
+        function erase()
+        {
             ctx.clearRect(0, 0, w, h);
-            document.getElementById("canvasimg").style.display = "none";
+
+            //as is, this throws a TypeError because that element does not exist. If it gets added, we should uncomment this
+            //document.getElementById("canvasimg").style.display = "none";
 
         }
     }
